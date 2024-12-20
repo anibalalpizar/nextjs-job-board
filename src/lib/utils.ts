4dd2fe5de58be211292785721005df72b6bdf346
anibalalpizar <anibal.alpizar14@gmail.com>
2024-12-20 02:48:17 -0600

    Add utility functions for currency formatting and relative date display

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { formatDistanceToNowStrict } from "date-fns"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function formatCurrency(amount: number, currency = "USD") {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+  }).format(amount)
+}
+
+export function formatRelativeDate(date: Date) {
+  return formatDistanceToNowStrict(date, { addSuffix: true })
 }
