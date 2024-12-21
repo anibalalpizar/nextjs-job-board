43e8e8d50743d23053dedbd0076fe37c21789f71
anibalalpizar <anibal.alpizar14@gmail.com>
2024-12-20 21:57:22 -0600

    Add job and location types constants for job listings

---
+++ src/constants/jobs.types.ts
@@ -0,0 +1,10 @@
+export const JOBS_TYPES = [
+  "Full-time",
+  "Part-time",
+  "Contract",
+  "Temporary",
+  "Internship",
+  "Volunteer",
+]
+
+export const LOCATIONS_TYPES = ["Remote", "On-site", "Hybrid"]
