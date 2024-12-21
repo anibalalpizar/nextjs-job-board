554db54c7cb6d519fe01d4093696b9ae79c9c151
anibalalpizar <anibal.alpizar14@gmail.com>
2024-12-20 23:20:18 -0600

    Add H1 component for consistent heading styling on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma"
 import JobListItem from "@/components/JobsListItem"
 import JobsFilterSidebar from "@/components/JobsFilterSidebar"
+import H1 from "@/components/ui/h1"
 
 export default async function Home() {
   const jobs = await prisma.job.findMany({
@@ -10,9 +11,7 @@ export default async function Home() {
   return (
     <main className="max-w-5xl m-auto px-3 my-10 space-y-10">
       <div className="space-y-5 text-center">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Find your next job
-        </h1>
+        <H1>Find your next job</H1>
         <p className="text-muted-foreground">
           Browse through thousands of full-time or part-time jobs near you or
           worldwide.
