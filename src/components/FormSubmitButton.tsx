63f320f9a9989b55c0ad9f28515c270208307cd5
anibalalpizar <anibal.alpizar14@gmail.com>
2024-12-25 23:58:55 -0600

    Refactor FormSubmitButton to use LoadingButton component for improved loading state handling

--- src/components/FormSubmitButton.tsx
+++ src/components/FormSubmitButton.tsx
@@ -1,20 +1,12 @@
 "use client"
 
 import { useFormStatus } from "react-dom"
-import { Loader } from "lucide-react"
 
-import { Button } from "./ui/button"
+import LoadingButton from "./LoadingButton"
 
 export default function FormSubmitButton(
   props: React.ButtonHTMLAttributes<HTMLButtonElement>
 ) {
   const { pending } = useFormStatus()
-  return (
-    <Button type="submit" disabled={props.disabled || pending} {...props}>
-      <span className="flex items-center justify-center gap-1">
-        {pending && <Loader size={16} className="animate-spin" />}
-        {props.children}
-      </span>
-    </Button>
-  )
+  return <LoadingButton {...props} type="submit" loading={pending} />
 }
